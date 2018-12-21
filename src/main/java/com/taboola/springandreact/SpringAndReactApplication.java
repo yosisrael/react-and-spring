396ac6b264e99c61e768e7ea46e42d5863b5a716
yos.i <yos.i@taboola.com>
2018-12-21 14:21:32 +0200

    first commit

---
+++ src/main/java/com/taboola/springandreact/SpringAndReactApplication.java
@@ -0,0 +1,14 @@
+package com.taboola.springandreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndReactApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/taboola/springandreact/SpringAndReactApplicationTests.java
@@ -0,0 +1,17 @@
+package com.taboola.springandreact;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAndReactApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
